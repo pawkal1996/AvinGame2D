6bfc6dc53863dd8021fa88ab708d31c165141766
Paweł Kałasiewicz <pawlus.kaasiewicz@gmail.com>
2018-12-03 21:45:56 +0100

    Abstract class for all characters - changed

--- src/Characters/Character.java
+++ src/Characters/Character.java
@@ -1,5 +1,27 @@
 package Characters;
 
 public abstract class Character {
+	private int experience;
+	private int attack;
+	private int defense;
+	
+	public int getExperience() {
+		return experience;
+	}
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	public int getDefense() {
+		return defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
 
 }
