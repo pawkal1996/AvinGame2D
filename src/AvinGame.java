a21fc7fda3dc0b89cbf845aab8f9b184d6af9dff
Paweł Kałasiewicz <pawlus.kaasiewicz@gmail.com>
2018-12-05 13:20:09 +0100

    Class Sword corrected

--- src/AvinGame.java
+++ src/AvinGame.java
@@ -1,8 +1,10 @@
+import items.Sword;
 
 public class AvinGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Sword.createSwords();
+		
 
 	}
 
--- src/items/Sword.java
+++ src/items/Sword.java
@@ -8,11 +8,13 @@ public class Sword extends Item {
 		this.attack = attack;
 	}
 	
+	public static void createSwords() {
 	Sword sword1 = new Sword("¯elazny sztylet", 10);
 	Sword sword2 = new Sword("Diabelska brzytwa", 20);
 	Sword sword3 = new Sword("Piekielny tasak", 30);
 	Sword sword4 = new Sword("Epicka halabarda", 40);
-
+	}
+	
 	private String name;
 	private int attack;
 	
